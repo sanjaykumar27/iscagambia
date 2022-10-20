2129b55725422af09529f783f69b43c7cf02c8c5
Sanjay <engineer.sanjay20@gmail.com>
2022-10-21 00:39:10 +0530

    ..

---
+++ dmxConnect/modules/Mailer/mail.php
@@ -0,0 +1,14 @@
+<?php
+$exports = <<<'JSON'
+{
+  "name": "mail",
+  "module": "mail",
+  "action": "setup",
+  "options": {
+    "password": "77AzcAK#l8)j",
+    "username": "info@iscagambia.com",
+    "host": "iscagambia.com"
+  }
+}
+JSON;
+?>
---
+++ test.php
@@ -0,0 +1,62 @@
+<!doctype html>
+<html>
+
+<head>
+  <base href="/">
+  <script src="dmxAppConnect/dmxAppConnect.js"></script>
+  <meta charset="UTF-8">
+  <title>Untitled Document</title>
+</head>
+
+<body is="dmx-app" id="test">
+  <table style="border:2px solid #000;margin-bottom:2px;border-bottom:0">
+    <tr>
+      <td>
+        <div style="height:290px;width:290px;background-color:#eceff6;border-right:1px;padding:15px">
+          <img style="height:65px" src="https://iscagambia.com/assets/images/logo.jpeg">
+          <p>Name: <strong>Sanjay Kumar</strong></p>
+          <p>Mobile: <strong>Sanjay Kumar</strong></p>
+          <p>Type: <strong>Sanjay Kumar</strong></p>
+          <p>Amount: <strong>Sanjay Kumar</strong></p>
+        </div>
+      </td>
+      <td><img style="height:320px;margin-left:-4px" src="https://iscagambia.com/assets/images/ticket.jpeg"></td>
+      <td>
+        <div style="height:290px;width:300px;background-color:#eceff6;border-right:1px;padding:15px;margin-left:-4px">
+          <h3 style="color:brown;text-transform:uppercase;text-align:center">LUCKY DIP TICKET</h3>
+          <h1 style="color:#ff8d00;text-transform:uppercase;text-align:center">SILVER</h1>
+          <p style="color:#000;margin-top:20px;text-transform:uppercase;text-align:center">TICKET NO:</p>
+          <p style="color:#ff8d00;font-size:22px;text-transform:uppercase;text-align:center;font-weight:600">S10</p>
+          <img style="height:65px;float:right" src="https://iscagambia.com/assets/images/logo.jpeg">
+        </div>
+      </td>
+    </tr>
+  </table>
+  <table style="border:2px solid #000;border-top:2px dashed #000">
+    <tr>
+      <td>
+        <div style="height:290px;width:290px;background-color:#eceff6;border-right:1px;padding:15px">
+          <p style="margin-bottom:0px">DUPLICATE</p><br>
+          <img style="height:65px" src="https://iscagambia.com/assets/images/logo.jpeg">
+          <p>Name: <strong>Sanjay Kumar</strong></p>
+          <p>Mobile: <strong>Sanjay Kumar</strong></p>
+          <p>Type: <strong>Sanjay Kumar</strong></p>
+          <p>Amount: <strong>Sanjay Kumar</strong></p>
+        </div>
+      </td>
+      <td><img style="height:320px;margin-left:-4px" src="https://iscagambia.com/assets/images/ticket.jpeg"></td>
+      <td>
+        <div style="height:290px;width:300px;background-color:#eceff6;border-right:1px;padding:15px;margin-left:-4px">
+          <p>DUPLICATE</p>
+          <h3 style="color:brown;text-transform:uppercase;text-align:center">LUCKY DIP TICKET</h3>
+          <h1 style="color:#ff8d00;text-transform:uppercase;text-align:center">SILVER</h1>
+          <p style="color:#000;margin-top:20px;text-transform:uppercase;text-align:center">TICKET NO:</p>
+          <p style="color:#ff8d00;font-size:22px;text-transform:uppercase;text-align:center;font-weight:600">S10</p>
+          <img style="height:65px;float:right" src="https://iscagambia.com/assets/images/logo.jpeg">
+        </div>
+      </td>
+    </tr>
+  </table>
+</body>
+
+</html>
